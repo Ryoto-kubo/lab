db8a5eeb48c39e0a4bf9fc143ae3ccc9878e2e3e
RyotoKubo <sax.guiter@icloud.com>
2020-11-21 13:01:00 +0900

    first commit

---
+++ src/components/pages/form/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <table>
+    <RowSet
+      title="名前"
+      placeholder="名前を入力してください"
+      :prop-value.sync="postObject.name"
+      @input="setName"
+    />
+    <RowSet
+      title="電話番号"
+      placeholder="090-1234-5678"
+      :prop-value.sync="postObject.tel"
+      @input="setTel"
+    />
+    <RowSet
+      title="住所"
+      placeholder="東京都港区2-2-2"
+      :prop-value.sync="postObject.address"
+      @input="setAddress"
+    />
+  </table>
+</template>
+
+<script>
+import RowSet from '../../parts/table/rowSet/index';
+export default {
+  components: {
+    RowSet
+  },
+  data() {
+    return {
+      postObject: {
+        name: '',
+        tel: '',
+        address: ''
+      }
+    };
+  },
+  methods: {
+    setName(value) {
+      this.postObject.name = value;
+    },
+    setTel(value) {
+      this.postObject.tel = value;
+    },
+    setAddress(value) {
+      this.postObject.address = value;
+    }
+  }
+};
+</script>
+
+<style></style>
---
+++ src/components/parts/table/rowSet/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <tr>
+    <td>{{ title }}</td>
+    <td>
+      <input type="text" :placeholder="placeholder" v-model="inputValue" />
+    </td>
+  </tr>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    placeholder: {
+      type: String,
+      required: true
+    },
+    propValue: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    inputValue: {
+      get() {
+        return this.$props.propValue;
+      },
+      set(value) {
+        this.$emit('input', value);
+      }
+    }
+  }
+};
+</script>
+
+<style></style>
