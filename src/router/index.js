7029b8b547954bcf61a66546bc08ce81566f30b0
RyotoKubo <sax.guiter@icloud.com>
2020-11-28 19:46:39 +0900

    test commit

--- src/router/index.js
+++ src/router/index.js
@@ -21,7 +21,7 @@ const routes = [
       import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
-    path: '/form',
+    path: '/form-create',
     name: 'Form',
     component: Form
   }
