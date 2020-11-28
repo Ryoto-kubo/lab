da541342ab7e06aadca6b7d22d1fcaf7e0b7ea33
RyotoKubo <sax.guiter@icloud.com>
2020-11-28 19:59:13 +0900

    test commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,75 +3,90 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier'],
   parserOptions: {
-    parser: "babel-eslint"
+    parser: 'babel-eslint'
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "vue/component-name-in-template-casing": ["error", "PascalCase"],
-    "vue/singleline-html-element-content-newline": "off",
-    "vue/html-self-closing": "off",
-    "vue/valid-template-root": "off",
-    "vue/no-unused-vars": "off",
-    "vue/valid-v-for": "off",
-    "vue/no-multi-spaces": ["error", { "ignoreProperties": false }],
-    "no-extra-parens": "warn",
-    "no-multi-spaces": "error",
-    "eqeqeq": ["error", "smart"],
-    "semi": ["error", "always"],
-    "quotes": ["error", "single"],
-    "block-spacing": ["error", "always"],
-    "comma-spacing": "error",
-    "computed-property-spacing": "error",
-    "arrow-spacing": ["error", {
-      "before": true,
-      "after": true
-    }],
-    "key-spacing": ["error", {
-      "beforeColon": false,
-      "afterColon": true,
-      "mode": "minimum"
-    }],
-    "func-call-spacing": ["error", "never"],
-    "no-spaced-func": "error",
-    "space-infix-ops": ["error", { "int32Hint": false }],
-    "no-multiple-empty-lines": ["error", { "max": 1 }],
-    "spaced-comment": ["error", "always", {
-      "line": {
-        "markers": ["/"],
-        "exceptions": ["-", "+"]
-      },
-      "block": {
-        "markers": ["!"],
-        "exceptions": ["*"],
-        "balanced": true
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/html-self-closing': 'off',
+    'vue/valid-template-root': 'off',
+    'vue/no-unused-vars': 'off',
+    'vue/valid-v-for': 'off',
+    'vue/no-multi-spaces': ['error', { ignoreProperties: false }],
+    'no-extra-parens': 'warn',
+    'no-multi-spaces': 'error',
+    eqeqeq: ['error', 'smart'],
+    semi: ['error', 'always'],
+    quotes: ['error', 'single'],
+    'block-spacing': ['error', 'always'],
+    'comma-spacing': 'error',
+    'computed-property-spacing': 'error',
+    'arrow-spacing': [
+      'error',
+      {
+        before: true,
+        after: true
       }
-    }],
-    "no-console": "off",
-    "space-in-parens": ["error", "never"],
-    "no-var": "error",
-    "space-before-function-paren": "off",
-    "space-before-blocks": ["error", {
-        "functions": "always",
-        "keywords": "always",
-        "classes": "always"
-    }],
-    "brace-style": ["error", "1tbs", { "allowSingleLine": true }],
-    "no-unused-vars": ["warn", { "vars": "all", "args": "after-used", "ignoreRestSiblings": false }],
-    "no-lonely-if": "error",
-    "no-trailing-spaces": "error",
-    "operator-linebreak": ["error", "before"],
-    "semi-spacing": "error",
-    "keyword-spacing": "error",
-    "arrow-parens": "off"
+    ],
+    'key-spacing': [
+      'error',
+      {
+        beforeColon: false,
+        afterColon: true,
+        mode: 'minimum'
+      }
+    ],
+    'func-call-spacing': ['error', 'never'],
+    'no-spaced-func': 'error',
+    'space-infix-ops': ['error', { int32Hint: false }],
+    'no-multiple-empty-lines': ['error', { max: 1 }],
+    'spaced-comment': [
+      'error',
+      'always',
+      {
+        line: {
+          markers: ['/'],
+          exceptions: ['-', '+']
+        },
+        block: {
+          markers: ['!'],
+          exceptions: ['*'],
+          balanced: true
+        }
+      }
+    ],
+    'space-in-parens': ['error', 'never'],
+    'no-var': 'error',
+    'space-before-function-paren': 'off',
+    'space-before-blocks': [
+      'error',
+      {
+        functions: 'always',
+        keywords: 'always',
+        classes: 'always'
+      }
+    ],
+    'brace-style': ['error', '1tbs', { allowSingleLine: true }],
+    'no-unused-vars': [
+      'warn',
+      { vars: 'all', args: 'after-used', ignoreRestSiblings: false }
+    ],
+    'no-lonely-if': 'error',
+    'no-trailing-spaces': 'error',
+    'operator-linebreak': ['error', 'before'],
+    'semi-spacing': 'error',
+    'keyword-spacing': 'error',
+    'arrow-parens': 'off'
   },
   overrides: [
     {
       files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)"
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
       ],
       env: {
         jest: true
